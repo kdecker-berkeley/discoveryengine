ef0022e6eb4683a6dcc397dddc210b279cfc4520
Caleb <cwolfsonseeley@berkeley.edu>
2016-03-23 12:35:44 -0700

    add has_affiliation widget

--- R/degree.R
+++ R/degree.R
@@ -1,5 +1,6 @@
 degree_ <- function(schools, levels = c("A", "U", "G", "L"), env = parent.frame()) {
     param <- schools
+    param <- resolve_codes(param, "school_code")
     p2 <- levels
     param1 <- substitute(school_code %in% param)
     param2 <- substitute(degree_level_code %in% p2)
@@ -10,4 +11,8 @@ degree <- function(..., levels = c("A", "U", "G", "L"), env = parent.frame()) {
     param <- pryr::dots(...)
     param <- prep_string_param(param, env = env)
     degree_(param, levels = levels, env = env)
+}
+
+resolve_codes <- function(codes, type) {
+  codes
 }
