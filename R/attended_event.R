0b9e49b33476febf7eb4efb2c0b82194bb7a2d27
Caleb <cwolfsonseeley@berkeley.edu>
2016-03-24 13:42:22 -0700

    change levels to status

--- R/attended_event.R
+++ R/attended_event.R
@@ -1,14 +1,14 @@
-attended_event_ <- function(events, levels = c("P"), env = parent.frame()) {
+attended_event_ <- function(events, participation = c("P"), env = parent.frame()) {
   param <- events
   param <- resolve_codes(param, "activity_code")
-  p2 <- levels
+  p2 <- participation
   param1 <- substitute(activity_code %in% param)
   param2 <- substitute(activity_participation_code %in% p2)
   bio_("activity", list(param1))
 }
 
-attended_event <- function(..., levels = c("P"), env = parent.frame()) {
+attended_event <- function(..., participation = c("P"), env = parent.frame()) {
   param <- pryr::dots(...)
   param <- prep_string_param(param, env = env)
-  attended_event_(param, levels = levels, env = env)
+  attended_event_(param, participation = participation, env = env)
 }
