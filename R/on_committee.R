998d5e9f0a7549bfcb94284eac4140ef57691dfb
Caleb <cwolfsonseeley@berkeley.edu>
2016-03-24 13:43:28 -0700

    change levels to status

--- R/on_committee.R
+++ R/on_committee.R
@@ -1,14 +1,14 @@
-on_committee_ <- function(committees, levels = c("A", "D"), env = parent.frame()) {
+on_committee_ <- function(committees, status = c("A", "D"), env = parent.frame()) {
   param <- committees
   param <- resolve_codes(param, "committee_code")
-  p2 <- levels
+  p2 <- status
   param1 <- substitute(committee_code %in% param)
   param2 <- substitute(status_code %in% p2)
   bio_("committee", list(param1))
 }
 
-on_committee <- function(..., levels = c("A", "D"), env = parent.frame()) {
+on_committee <- function(..., status = c("A", "D"), env = parent.frame()) {
   param <- pryr::dots(...)
   param <- prep_string_param(param, env = env)
-  on_committee_(param, levels = levels, env = env)
+  on_committee_(param, status = status, env = env)
 }
