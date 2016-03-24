87d1e4f62857e65a5d411dbd5f51234b40d33f8e
Caleb <cwolfsonseeley@berkeley.edu>
2016-03-24 13:40:23 -0700

    change levels to status

--- R/has_affiliation.R
+++ R/has_affiliation.R
@@ -1,14 +1,14 @@
-has_affiliation_ <- function(affils, levels = c("C", "F"), env = parent.frame()) {
+has_affiliation_ <- function(affils, status = c("C", "F"), env = parent.frame()) {
   param <- affils
   param <- resolve_codes(param, "affil_code")
-  p2 <- levels
+  p2 <- status
   param1 <- substitute(affil_code %in% param)
   param2 <- substitute(affil_status_code %in% p2)
   bio_("affiliation", list(param1))
 }
 
-has_affiliation <- function(..., levels = c("C", "F"), env = parent.frame()) {
+has_affiliation <- function(..., status = c("C", "F"), env = parent.frame()) {
   param <- pryr::dots(...)
   param <- prep_string_param(param, env = env)
-  has_affiliation_(param, levels = levels, env = env)
+  has_affiliation_(param, status = status, env = env)
 }
