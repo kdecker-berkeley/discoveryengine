2eb2161d98d43f9ee7ba9988ebee360138c91bcf
Caleb <cwolfsonseeley@berkeley.edu>
2016-04-14 09:01:03 -0700

    change to home only

--- R/lives_in_msa.R
+++ R/lives_in_msa.R
@@ -1,14 +1,16 @@
-lives_in_msa_ <- function(msa, status = c("A"), env = parent.frame()) {
+lives_in_msa_ <- function(msa, type = c("H"), env = parent.frame()) {
   param <- msa
   param <- resolve_codes(param, "geo_metro_area_code")
-  p2 <- status
+  p2 <- type
   param1 <- substitute(geo_metro_area_code %in% param)
-  param2 <- substitute(addr_status_code %in% p2)
+  param2 <- substitute(addr_type_code %in% p2)
   bio_("address", list(param1))
 }
 
-lives_in_msa <- function(..., status = c("A"), env = parent.frame()) {
+lives_in_msa <- function(..., status = c("H"), env = parent.frame()) {
   param <- pryr::dots(...)
   param <- prep_string_param(param, env = env)
   address_in_(param, status = status, env = env)
 }
+
+
