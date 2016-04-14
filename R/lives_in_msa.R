cc6cbc15f2955234112e11f9c1095e7477dac2c0
Caleb <cwolfsonseeley@berkeley.edu>
2016-04-14 08:37:18 -0700

    change addreess_in to lives_in_msa

--- R/lives_in_msa.R
+++ R/lives_in_msa.R
@@ -1,4 +1,4 @@
-address_in_ <- function(msa, status = c("A"), env = parent.frame()) {
+lives_in_msa_ <- function(msa, status = c("A"), env = parent.frame()) {
   param <- msa
   param <- resolve_codes(param, "geo_metro_area_code")
   p2 <- status
@@ -7,7 +7,7 @@ address_in_ <- function(msa, status = c("A"), env = parent.frame()) {
   bio_("address", list(param1))
 }
 
-address_in <- function(..., status = c("A"), env = parent.frame()) {
+lives_in_msa <- function(..., status = c("A"), env = parent.frame()) {
   param <- pryr::dots(...)
   param <- prep_string_param(param, env = env)
   address_in_(param, status = status, env = env)
