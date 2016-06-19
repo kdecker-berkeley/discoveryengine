fcd34ea18514a2b72df6bdff21d7d1eeb3d83600
tarakc02 <tarakc02@github.com>
2016-06-19 09:31:29 -0700

    make switches more semantically relevent
    eg: undergraduates = TRUE instead of levels = "U"

--- R/widget-on-committee.R
+++ R/widget-on-committee.R
@@ -1,13 +1,18 @@
 #' @export
-on_committee <- function(..., status = c("A", "D")) {
+on_committee <- function(..., include_former = TRUE) {
     committees <- prep_dots(...)
-    reroute(on_committee_(committees, status = status))
+    reroute(on_committee_(committees, include_former = include_former))
 }
 
-on_committee_ <- function(committees, status = c("A", "D")) {
+on_committee_ <- function(committees, include_former = TRUE) {
+    if (!is.logical(include_former))
+        stop("include_former must be TRUE or FALSE")
+    if (include_former) status_code <- c("C", "F")
+    else status_code <- "C"
+
     entity_widget("d_bio_committee_mv",
                   parameter = string_param("committee_code", committees),
-                  switches = string_switch("status_code", status))
+                  switches = string_switch("committee_status_code", status_code))
 }
 
 ## add role as another level
