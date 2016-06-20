e183df6e1b414b789689a9744002f3d423423500
tarakc02 <tarakc02@users.noreply.github.com>
2016-06-20 12:43:52 -0700

    export infix operators, shouldn't need to load listbuilder explicitly

--- R/listbuilder-reexports.R
+++ R/listbuilder-reexports.R
@@ -5,3 +5,15 @@ get_cdw <- listbuilder::get_cdw
 #' @importFrom listbuilder to_sql
 #' @export
 to_sql <- listbuilder::to_sql
+
+#' @importFrom listbuilder %and%
+#' @export
+`%and%` <- listbuilder::`%and%`
+
+#' @importFrom listbuilder %or%
+#' @export
+`%or%` <- listbuilder::`%or%`
+
+#' @importFrom listbuilder %minus%
+#' @export
+`%minus%` <- listbuilder::`%minus%`
