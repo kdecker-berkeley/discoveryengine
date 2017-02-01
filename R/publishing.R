382c94ca022570ea0924b307ff3b4a63d8372171
tarakc02 <tarakc02@users.noreply.github.com>
2017-02-01 10:10:45 -0800

    ability to publish

--- R/publishing.R
+++ R/publishing.R
@@ -1,33 +1,47 @@
-# has_music_interest <- has_interest(
-#     band_music,
-#     music,
-#     recital_music,
-#     world_music,
-#     classical_music,
-#     contemporary_music,
-#     early_music
-# )
+new_archive <- function() {
+    db <- structure(
+        new.env(parent = emptyenv()),
+        class = c("archive", "environment")
+    )
+
+}
+
+singles_db <- function() readRDS("R:/Prospect Development/Prospect Analysis/disco-singles/db.discoveryengine")
+save_singles_db <- function(db) saveRDS(db, file = "R:/Prospect Development/Prospect Analysis/disco-singles/db.discoveryengine")
 
 disco_single <- function(
     predicate,
-    author = "Tarak",
+    author = NULL,
     date = Sys.Date(),
-    description = "Dummy description",
-    keywords = c("key", "words", "etc"),
+    description = NULL,
+    keywords = NULL,
     name = NULL
 ) {
-    stopifnot(
-        inherits(predicate, "listbuilder"),
-        is.character(author),
-        is.character(description),
-        is.character(keywords),
-        length(author) == 1L,
-        length(description) == 1L,
-        !is.na(author),
-        !is.na(description)
-    )
+    stopifnot(inherits(predicate, "listbuilder"))
 
-    if (is.null(name)) name <- deparse(substitute(predicate))
+    if (is.null(author))
+        author <- readline("Enter the author's name: ")
+    assertthat::assert_that(assertthat::is.string(author), !is.na(author))
+
+    if (is.null(name)) {
+        cat(stringr::str_wrap(paste(
+            "Your single will need a name. Names should be valid R names, so
+            no spaces or names starting with numbers."),
+            width = 80L))
+        name <- readline("Give your new single a catchy name: ")
+    }
+    assertthat::assert_that(assertthat::is.string(name), !is.na(name))
+    goodname <- make.names(name)
+    if (goodname != name)
+        stop("Invalid name. Maybe something like: ", goodname, "?")
+
+    if (is.null(description))
+        description <- readline("Briefly describe what the single does: ")
+    assertthat::assert_that(assertthat::is.string(description), !is.na(description))
+
+    if (is.null(keywords))
+        keywords <- trimws(strsplit(readline("Enter some keywords (separated by commas) to help find your single: "), ",")[[1]], "both")
+    stopifnot(is.character(keywords), !any(is.na(keywords)))
 
     structure(
         list(
@@ -41,36 +55,38 @@ disco_single <- function(
     )
 }
 
-print.disco_single <- function(x, ...) {
+# print.disco_single <- function(x, ...) {
+#     cat(x$name, ": A disco single, by ", x$author, "\n", sep = "")
+#     cat("What it does:", x$description, "\n")
+#     invisible(x)
+# }
 
-}
 
-new_archive <- function() {
-    structure(
-        new.env(parent = emptyenv()),
-        class = c("archive", "environment")
-    )
-}
+publish <- function(single, ...) UseMethod("publish")
 
-push_single <- function(archive, single) UseMethod("push_single")
-list_singles <- function(archive) UseMethod("list_singles")
-find_singles <- function(archive, search_term) UseMethod("find_singles")
-
-push_single.archive <- function(archive, single) {
-    stopifnot(inherits(single, "disco_single"))
-    key <- digest::digest(single$predicate, algo = "md5")
-    key <- stringr::str_sub(key, -7)
-    if (exists(key, archive)) {
-        old_single <- get(key, archive)
+publish.disco_single <- function(single, ...) {
+    # note: if two people try to do this at the same time, only one write
+    # will happen -- need to lock the file
+    archive <- singles_db()
+    if (exists(single$name, archive)) {
+        old_single <- get(single$name, archive)
         msg <- paste0("It seems this single already exists! It was created by ",
                       old_single$author,  " on ", old_single$date)
         stop(msg)
     }
-    assign(key, single, pos = archive)
-    return(key)
+    assign(single$name, single, pos = archive)
+    save_singles_db(archive)
+    message("Congrats!")
+    invisible(NULL)
 }
 
-list_singles.archive <- function(archive) {
+publish.listbuilder <- function(single, ...) {
+    single <- disco_single(single, ...)
+    publish(single)
+}
+
+list_singles <- function() {
+    archive <- singles_db()
     ids <- ls(archive)
     singles <- mget(ids, archive)
     summarize_single <- function(single)
@@ -81,7 +97,8 @@ list_singles.archive <- function(archive) {
     res
 }
 
-find_singles.archive <- function(archive, search_term) {
+find_singles <- function(search_term) {
+    archive <- singles_db()
     ids <- ls(archive)
     singles <- mget(ids, archive)
 
@@ -99,4 +116,9 @@ find_singles.archive <- function(archive, search_term) {
     if (length(res) <= 0L) stop("No singles found that match the search term '",
                                 search_term, "'")
     res
+}
+
+
+check_out <- function(single) {
+    eval(substitute(single), envir = singles_db())
 }
