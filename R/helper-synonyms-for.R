c4ab6212c487df8e401f7f2a77a4216c23e8504b
tarakc02 <tarakc02@users.noreply.github.com>
2017-02-06 16:42:21 -0800

    consistent synonyms for MSA
    MSA code table is not part of TMS table (why???)

--- R/helper-synonyms-for.R
+++ R/helper-synonyms-for.R
@@ -6,16 +6,25 @@ synonyms_for <- function(type) {
     if (nrow(dict_row) >= 1L) {
         tms <- unique(dict_row[["tms"]])[[1]]
 
-        qry <-
-            "select distinct
+        if (tms == "geo_code") {
+            qry <- "
+            select distinct
+                geo_code as code,
+                lower(regexp_replace(regexp_replace(trim(geo_code_description), '([^A-Za-z0-9 //_-])', ''), '((( )+)|-|(\\/))+', '_')) as syn
+            from cdw.d_geo_metro_area_mv where geo_type = 1"
+            syn_df <- getcdw::get_cdw(qry)
+        } else {
+            qry <-
+                "select distinct
             tms_type_code as code,
             lower(regexp_replace(regexp_replace(trim(tms_type_desc), '([^A-Za-z0-9 //_-])', ''), '((( )+)|-|(\\/))+', '_')) as syn
             from CDW.d_tms_type_mv
             where tms_view_name = '##tms##'
             and tms_type_desc not like '*%'"
 
-        qry <- getcdw::parameterize_template(qry)
-        syn_df <- getcdw::get_cdw(qry(tms = tms))
+            qry <- getcdw::parameterize_template(qry)
+            syn_df <- getcdw::get_cdw(qry(tms = tms))
+        }
 
         # these are synonyms automatically generated from tms table
         res <- structure(syn_df$code, names = syn_df$syn)
