5c8a19e4faedfc4430f6b43720bc8446b31cb4e3
tarakc02 <tarakc02@users.noreply.github.com>
2017-04-07 17:35:17 -0700

    more detailed startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,17 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
     msg <- "Welcome to the Disco Engine! If you're not sure where to start, check out the cheat sheet: https://tarakc02.github.io/discodocs/cheat-sheet.html"
+    msg <- paste0(
+        msg, "  You are using version ", packageVersion("discoveryengine"),
+        " of the Disco Engine. To see what the latest version number is, ",
+        "and to see the list of updates and bugfixes in each version, ",
+        "see https://github.com/tarakc02/discoveryengine/blob/master/NEWS.md")
 
-    packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
+    packageStartupMessage(
+        paste(
+            strwrap(
+                msg, exdent = 2,
+                width = pmin(.9 * getOption("width"), 80)),
+            collapse = "\n"
+        )
+    )
 }
