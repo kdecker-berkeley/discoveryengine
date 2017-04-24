75abea1e2c040123334a9a4d8ec0e44e2abf62bb
tarakc02 <tarakc02@users.noreply.github.com>
2017-04-24 15:49:31 -0700

    simplify startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,8 @@
 .onAttach <- function(libname, pkgname) {
-    msg <- "Welcome to the Disco Engine! If you're not sure where to start, check out the cheat sheet: https://tarakc02.github.io/discodocs/cheat-sheet.html"
     msg <- paste0(
-        msg, "  You are using version ", packageVersion("discoveryengine"),
-        " of the Disco Engine. To see what the latest version number is, ",
-        "and to see the list of updates and bugfixes in each version, ",
-        "see https://github.com/tarakc02/discoveryengine/blob/master/NEWS.md")
-
-    packageStartupMessage(
-        paste(
-            strwrap(
-                msg, exdent = 2,
-                width = pmin(.9 * getOption("width"), 80)),
-            collapse = "\n"
-        )
+        "Welcome to Disco Engine version ", packageVersion("discoveryengine"), "\n",
+        "Cheat sheet: https://tarakc02.github.io/discodocs/cheat-sheet.html", "\n",
+        "News and bugfixes: https://github.com/tarakc02/discoveryengine/blob/master/NEWS.md"
     )
+    packageStartupMessage(msg)
 }
