26b3d89d7a6c93ea15e227a6bb6670ee3798fd38
tarakc02 <tarakc02@users.noreply.github.com>
2018-01-16 15:36:39 -0800

    unit test for married_to, see discussion of #57

---
+++ tests/testthat/test-married-to.R
@@ -0,0 +1,22 @@
+context("married_to")
+
+test_that("married_to correctly gets spouses", {
+    expect_equivalent(display(married_to(727565)), data.frame(entity_id = 727558))
+})
+
+# cnr_degreeholder = has_degree_from(NR)
+# cnr_donor = gave_to_area(CNR, at_least = 5000)
+#
+# library(tidyverse)
+#
+# old_version <- display(cnr_donor %but_not% cnr_degreeholder)
+#
+#
+# new_version <- display(
+#     cnr_donor %but_not%
+#         (cnr_degreeholder %or% married_to(cnr_degreeholder))
+# )
+#
+# setdiff(old_version, new_version) # just spouses, as expected
+# setdiff(new_version, old_version) # no results (as expected)
+
