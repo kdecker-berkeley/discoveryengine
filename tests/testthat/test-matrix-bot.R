d238693de4c694f3eb1fd1b35838b4bd0843d667
tarakc02 <tarakc02@users.noreply.github.com>
2018-01-18 15:26:51 -0800

    unit test to replicate issue #59

--- tests/testthat/test-matrix-bot.R
+++ tests/testthat/test-matrix-bot.R
@@ -8,3 +8,7 @@ test_that("matrix bot returns something when something is expected", {
 test_that("matrix bot errors with a message when nothing should come up", {
     expect_error(matrix_bot(gave_to_area(CHAN), "didn't find anything"))
 })
+
+test_that("matrix bot gives meaningful error message when non-entity definition is supplied", {
+    expect_error(matrix_bot(contact_text_contains("neuro*")), "entity_id")
+})
