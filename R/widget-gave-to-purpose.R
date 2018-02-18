fc30215e8b8914cac0fde6122e2b306b20ef0538
Tarak Shah <tarak_shah@berkeley.edu>
2018-02-18 09:28:01 -0800

    fix gave_to_purpose

--- R/widget-gave-to-purpose.R
+++ R/widget-gave-to-purpose.R
@@ -3,8 +3,8 @@
 gave_to_purpose <- function(..., at_least = 0.01, from = NULL,
                               to = NULL) {
     purps <- prep_dots(...)
-    reroute(gave_to_fund_purpose_(purps, at_least = at_least,
-                                  from = from, to = to))
+    reroute(gave_to_purpose_(purps, at_least = at_least,
+                             from = from, to = to))
 }
 
 gave_to_purpose_ <- function(purps, at_least = 0.01, from = NULL, to = NULL) {
