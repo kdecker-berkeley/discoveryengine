2051c29a062cd95d16ddc9d6ebeed23175245c5e
tarakc02 <tarakc02@users.noreply.github.com>
2018-06-20 14:34:41 -0700

    update tests for CA candidate widget after updating ID calculations

--- tests/testthat/test-ca-gave-to-candidate.R
+++ tests/testthat/test-ca-gave-to-candidate.R
@@ -3,13 +3,11 @@ source("helpers.R")
 library(magrittr)
 
 test_that("ca_gave_to_candidate meets specifications on standard input", {
-    test <- ca_gave_to_candidate(CA83588497, CA83971300)
+    test <- ca_gave_to_candidate(CA919925929, CA2604502777, CA1303768232)
     test %>% uses_table("ca_campaign")
     test %>% id_of_type("entity_id")
     test %>% id_field_is("entity_id")
 
     test$rhs %>%
-        has_filters(candidate_id = c("CA83588497", "CA83971300"))
-
-
+        has_filters(candidate_id = c("CA919925929", "CA2604502777", "CA1303768232"))
 })
