ab07da0358eac281d95bab1caa22f7073b983e59
tarakc02 <tarakc02@users.noreply.github.com>
2018-06-20 14:37:12 -0700

    allow widgets from inline queries instead of tables

--- R/helper-widget-builder.R
+++ R/helper-widget-builder.R
@@ -14,3 +14,19 @@ widget_builder <- function(table, id_field, id_type, parameter = NULL,
                               schema = schema)
 }
 
+widget_builder_custom <- function(
+    custom, id_field, id_type, parameter = NULL,
+    aggregate_parameter = NULL,
+    switches = NULL, aggregate_switches = NULL,
+    schema = "CDW") {
+
+    logic <- build_logic(parameter, aggregate_parameter,
+                         switches, aggregate_switches)
+
+    listbuilder::custom_query(
+        custom = custom,
+        where = logic$where,
+        having = logic$having,
+        id_field = id_field,
+        id_type = id_type)
+}
