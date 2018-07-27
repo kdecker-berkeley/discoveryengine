6b6693f3749fb93c99a6995155610e3321fdf6a1
tarakc02 <tarakc02@users.noreply.github.com>
2018-07-26 18:46:42 -0700

    has_degree doesn't need options for grad/undergrad, as that is implied by the degree type

--- R/widget-has-degree.R
+++ R/widget-has-degree.R
@@ -1,29 +1,30 @@
 #' @rdname academic
 #' @export
-has_degree <- function(..., undergraduates = TRUE,
-                       graduates = TRUE, attendees= FALSE,
+has_degree <- function(...,
+                       attendees= FALSE,
                        current_students = FALSE,
                        degreeholders = TRUE,
                        from = NULL, to = NULL,
                        advisor = NULL) {
     codes <- prep_dots(...)
-    reroute(has_degree_(codes, undergraduates = undergraduates,
-                        graduates = graduates, attendees = attendees,
+    reroute(has_degree_(codes,
+                        attendees = attendees,
                         current_students = current_students,
                         degreeholders = degreeholders,
                         from = from, to = to,
                         advisor = advisor))
 }
 
-has_degree_ <- function(codes, undergraduates = TRUE,
-                        graduates = TRUE, attendees = FALSE,
+has_degree_ <- function(codes,
+                        attendees = FALSE,
                         current_students = FALSE,
                         degreeholders = TRUE,
                         from = NULL, to = NULL,
                         advisor = NULL) {
+
     academic_widget(param = string_param("degree_code", codes),
-                    undergraduates = undergraduates,
-                    graduates = graduates,
+                    undergraduates = TRUE,
+                    graduates = TRUE,
                     attendees = attendees,
                     current_students = current_students,
                     degreeholders = degreeholders,
