d4df8a88e3c5d46a3500a25934a52175eedf5049
tarakc02 <tarakc02@users.noreply.github.com>
2018-08-24 13:28:58 -0700

    param operator should be determined before any further transformations

--- R/params-integer-param.R
+++ R/params-integer-param.R
@@ -1,10 +1,10 @@
 integer_param <- function(field_name, ints, default = NULL) {
     codelist <- prep_integer_param(ints)
+    operator <- param_operator(codelist)
     if (length(codelist) <= 0) {
         if (is.language(default)) return(default)
         else codelist <- default
     }
-    operator <- param_operator(codelist)
 
     # params will be of the form x %in% y
     if (length(codelist) > 0) {
