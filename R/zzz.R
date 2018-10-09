1698a90c8d73d98a87ff3faf481a9011fbf9d980
tarakc02 <tarakc02@users.noreply.github.com>
2018-10-09 15:04:10 -0700

    update links to point to new repo/docs locations (cwolfsonseeley instead of tarakc02)

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
     msg <- paste0(
         "Welcome to Disco Engine version ", packageVersion("discoveryengine"), "\n",
-        "Cheat sheet: https://tarakc02.github.io/discodocs/cheat-sheet.html", "\n",
-        "News and bugfixes: https://github.com/tarakc02/discoveryengine/blob/master/NEWS.md"
+        "Cheat sheet: https://cwolfsonseeley.github.io/discodocs/cheat-sheet.html", "\n",
+        "News and bugfixes: https://github.com/cwolfsonseeley/discoveryengine/blob/master/NEWS.md"
     )
     packageStartupMessage(msg)
 }
