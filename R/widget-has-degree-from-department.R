cd9aa2ddff7e6e8d0d4d1d36cb9eaf18071d4d17
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2020-02-24 11:40:13 -0500

    add department widget

---
+++ R/widget-has-degree-from-department.R
@@ -0,0 +1,33 @@
+#' @rdname academic
+#' @export
+has_degree_from_department <- function(..., undergraduates = TRUE,
+                       graduates = TRUE, attendees= FALSE,
+                       current_students = FALSE,
+                       degreeholders = TRUE,
+                       from = NULL, to = NULL,
+                       advisor = NULL)
+    reroute(has_degree_from_department_(
+        prep_dots(...), undergraduates = undergraduates,
+        graduates = graduates, attendees = attendees,
+        current_students = current_students,
+        degreeholders = degreeholders,
+        from = from, to = to,
+        advisor = advisor))
+
+has_degree_from_department_ <- function(departments, undergraduates = TRUE,
+                        graduates = TRUE, attendees= FALSE,
+                        current_students = FALSE,
+                        degreeholders = TRUE,
+                        from = NULL, to = NULL,
+                        advisor = NULL) {
+
+    academic_widget(param = string_param("dept_code", departments, width = 3),
+                    undergraduates = undergraduates,
+                    graduates = graduates,
+                    attendees = attendees,
+                    current_students = current_students,
+                    degreeholders = degreeholders,
+                    from = from, to = to,
+                    advisor = advisor)
+}
+
