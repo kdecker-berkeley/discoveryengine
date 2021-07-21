e5f421b257316f24688bb8aea918e9fc9a4f1498
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2021-07-21 13:26:15 -0400

    add non-UC academic widgets

---
+++ R/helper-nonuc-academic-widgets.R
@@ -0,0 +1,59 @@
+nonuc_academic_widget <- function(
+    param,
+    undergraduates, graduates, attendees, current_students, degreeholders,
+    from, to, advisor) {
+
+    levels = NULL
+    student_levels <- NULL
+    date_switch <- daterange(
+        dbplyr::sql("case when degree_level_code in ('U', 'G') then grad_dt else stop_dt end"),
+        from = from, to = to
+    )
+
+    if (!undergraduates && !graduates)
+        stop("You must include at least one of undergraduates or graduates, ",
+             "but they are both FALSE")
+
+    if (!attendees && !current_students && !degreeholders)
+        stop("You must include at least one of attendees, ",
+             "current_students, or degreeholders, but they are all FALSE")
+
+    if (undergraduates) {
+        if (degreeholders) levels <- c(levels, "U")
+        if (attendees) levels <- c(levels, "A")
+        if (current_students) student_levels <- c(student_levels, "A")
+    }
+
+    if (graduates) {
+        if (degreeholders) levels <- c(levels, "G")
+        if (attendees) levels <- c(levels, c("L", "V", "P"))
+        if (current_students) student_levels <- c(student_levels, "L")
+    }
+
+    if (any(degreeholders, attendees)) {
+        alum <- entity_widget("d_bio_degrees_mv",
+                              parameter = param,
+                              switches = list(
+                                  string_switch("degree_level_code", levels),
+                                  date_switch
+                              ))
+    } else {
+        alum <- NULL
+    }
+    if (current_students) {
+        if (is.list(param)) st_param <- param[[1]] else st_param <- param
+        if (!is.null(st_param) && st_param[[2]] == quote(degree_code))
+            st_param[[2]] <- quote(case_code)
+        st <- entity_widget("d_bio_degrees_mv",
+                            parameter = st_param,
+                            switches = list(
+                                string_switch("degree_level_code", student_levels),
+                                string_switch("non_grad_code", c("C", "N", "R"))
+                            ))
+    } else {
+        st <- NULL
+    }
+
+    res <- Filter(function(x) !is.null(x), list(alum, st))
+    Reduce(`%or%`, res)
+}
---
+++ R/widget-has-external-degree-from.R
@@ -0,0 +1,68 @@
+#' Non-UC academic widgets
+#'
+#' Find entities who graduated from a particular institution. By default, attendees are not included, use \code{attendees = TRUE}
+#' to include attendees.
+#'
+#' @return A definition of type \code{entity_id}
+#'
+#' @param ... Institutions
+#' @param undergraduates TRUE/FALSE: should include undergraduates? Default is TRUE
+#' @param graduates TRUE/FALSE: should include graduates? Default is TRUE
+#' @param attendees TRUE/FALSE: should include attendees? Default is FALSE
+#' @param current_students TRUE/FALSE: should include current students? Default is FALSE
+#' @param degreeholders TRUE/FALSE: should include degreeholders? Default is TRUE
+#' @param from (optional) date range: look only for those who graduated between
+#' these dates. Enter as an integer of the form YYYYMMDD (see details)
+#' @param to (optional) date range: look only for those who graduated between
+#' these dates. Enter as an integer of the form YYYYMMDD (see details)
+#'
+#' When using the daterange (\code{from} and/or \code{to}), the dates will be
+#' based on the GRAD DATE for degreeholders and the STOP DATE for attendees
+#' (who have no GRAD DATE). Both dates are visible in the Degrees screen in
+#' Advance. An alum's "class year" is based on the GRAD DATE. For degreeholders,
+#' the two dates are often the same, though they can differ. See examples below.
+#' Date options are ignored when looking for current students
+#'
+#' @examples
+#' ## graduated from University of Michigan Ann Arbor between 01/01/2001 and 12/31/2004
+#' ## since attendees are not included by default, this only pulls
+#' ## (undergrad and grad) degreeholders. Those with a GRAD DATE in the
+#' ## daterange will be included
+#' has_external_degree_from(001839, from = 20010101, to = 20041231)
+#'
+#' ## just grad degree holders from University of Michigan Ann Arbor
+#' has_external_degree_from(001839, undergraduates = FALSE)
+#'
+#' @name non-uc-academic
+#' @seealso \code{\link{academic}}
+NULL
+
+#' @rdname non-uc-academic
+#' @export
+has_external_degree_from <- function(..., undergraduates = TRUE,
+                            graduates = TRUE, attendees= FALSE,
+                            current_students = FALSE,
+                            degreeholders = TRUE,
+                            from = NULL, to = NULL) {
+    institutions <- prep_dots(...)
+    reroute(has_degree_from_(institutions, undergraduates = undergraduates,
+                             graduates = graduates, attendees = attendees,
+                             current_students = current_students,
+                             degreeholders = degreeholders,
+                             from = from, to = to))
+}
+
+has_degree_from_ <- function(institutions, undergraduates = TRUE,
+                             graduates = TRUE, attendees = FALSE,
+                             current_students = FALSE,
+                             degreeholders = TRUE,
+                             from = NULL, to = NULL) {
+
+    nonuc_academic_widget(param = string_param("institution_code", institutions),
+                    undergraduates = undergraduates,
+                    graduates = graduates,
+                    attendees = attendees,
+                    current_students = current_students,
+                    degreeholders = degreeholders,
+                    from = from, to = to)
+}
