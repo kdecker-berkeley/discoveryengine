bd00ce3647d239ef04e9bd2200ccaf3ba4c630a5
cwolfsonseeley <cwolfsonseeley@berkeley.edu>
2021-08-05 12:26:41 -0400

    Update widget-has-degree-from.R
    fix typo

--- R/widget-has-degree-from.R
+++ R/widget-has-degree-from.R
@@ -70,14 +70,14 @@ has_degree_from <- function(..., undergraduates = TRUE,
                                        current_students = FALSE,
                                        degreeholders = TRUE,
                                        from = NULL, to = NULL,
-                                       advisor = NULL)
+                                       advisor = NULL) {
   reroute(has_degree_from_(
     prep_dots(...), undergraduates = undergraduates,
     graduates = graduates, attendees = attendees,
     current_students = current_students,
     degreeholders = degreeholders,
     from = from, to = to,
-    advisor = advisor))
+    advisor = advisor)) }
 
 has_degree_from_ <- function(schools, undergraduates = TRUE,
                                         graduates = TRUE, attendees= FALSE,
